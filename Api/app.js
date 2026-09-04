56f5b5ee5cc22191cd092bd6e666fe5e23241fee
Ayush <choudharyayush18@gmail.com>
2026-09-04 08:28:21 +0530

    Fix API CORS and Render port

--- Api/app.js
+++ Api/app.js
@@ -10,7 +10,12 @@ import messageRoute from "./routes/message.route.js";
 
 const app = express();
 
-app.use(cors({origin: "http://localhost:5173", credentials: true}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(express.json())
 app.use(cookieParser())
 
@@ -21,6 +26,6 @@ app.use("/api/users", userRoute);
 app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
 
-app.listen(8800, ()=>{
-  console.log("Server is running !");
+app.listen(process.env.PORT || 8800, () => {
+  console.log("Server is running!");
 });
